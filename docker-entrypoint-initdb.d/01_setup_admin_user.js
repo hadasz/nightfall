474b692d7aaee77349ceb43077ae350d97587bb4
Liju Jose <liju.jose@gds.ey.com>
2019-10-15 13:09:47 +0530

    7

--- docker-entrypoint-initdb.d/01_setup_admin_user.js
+++ docker-entrypoint-initdb.d/01_setup_admin_user.js
@@ -1,9 +1,22 @@
-this.db.createUser({
-  user: 'admin',
-  pwd: 'admin',
-  roles: [
-    { role: 'userAdmin', db: 'nightfall' },
-    { role: 'dbAdmin', db: 'nightfall' },
-    { role: 'readWrite', db: 'nightfall' },
-  ],
-});
+if (this.db.getName() === 'nightfall_test') {
+  // user created for integration testing.
+  this.db.createUser({
+    user: 'admin',
+    pwd: 'admin',
+    roles: [
+      { role: 'userAdmin', db: 'nightfall_test' },
+      { role: 'dbAdmin', db: 'nightfall_test' },
+      { role: 'readWrite', db: 'nightfall_test' },
+    ],
+  });
+} else {
+  this.db.createUser({
+    user: 'admin',
+    pwd: 'admin',
+    roles: [
+      { role: 'userAdmin', db: 'nightfall' },
+      { role: 'dbAdmin', db: 'nightfall' },
+      { role: 'readWrite', db: 'nightfall' },
+    ],
+  });
+}
